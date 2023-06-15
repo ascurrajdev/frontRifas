4ed8ab2973861cfb93f6b37dd7ec5717782c23dc
Jose Ascurra <joseascurra123@gmail.com>
2023-06-15 16:12:04 -0400

    Cambios en el login

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,18 +1,24 @@
-import { Card, Form, Input, Button } from "antd"
+import { Card, Form, Input, Button, Row, Col } from "antd"
 export const LoginScreen = () => {
     return(
-        <Card>
-            <Form>
-                <Form.Item name="email" label="Email">
-                    <Input />
-                </Form.Item>
-                <Form.Item name="password" label="Password">
-                    <Input />
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">Enviar</Button>
-                </Form.Item>
-            </Form>
-        </Card>
+        <Row>
+            <Col lg={{span:12, offset:6}} xs={{span:24}} sm={{span:24}}>
+                <Card>
+                    <Form
+                        layout="vertical"
+                    >
+                        <Form.Item name="email" label="Email">
+                            <Input placeholder="example@example.com" />
+                        </Form.Item>
+                        <Form.Item name="password" label="Password">
+                            <Input.Password placeholder="12345"/>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">Enviar</Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </Col>
+        </Row>
     )
 }
