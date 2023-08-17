8021c9be25be924f5136c68e98d4fc3c5a9718ea
Jose Ascurra <joseascurra123@gmail.com>
2023-08-17 08:52:43 -0400

    Agregado de los estilos iniciales para el listado de rifas

--- src/screens/raffles/ListRafflesScreen.tsx
+++ src/screens/raffles/ListRafflesScreen.tsx
@@ -1,5 +1,17 @@
+import { Card, Switch, } from "antd"
+
 export const ListRafflesScreen = () => {
     return (
-        <h1>Hola que tal</h1>
+        <div>
+            <Card
+                hoverable
+                style={{ width:240 }}
+                cover={<img alt="imagen de la rifa" src="https://image.slidesharecdn.com/opencontenthistory-1222747121590635-9/95/ten-years-of-open-content-21-728.jpg?cb=1222722154"/>}
+            >
+                <Card.Meta title="Pollada de Beneficiencia"/>
+                <Card.Meta description="Gs. 20.000" />
+                <Switch checkedChildren="Activo" unCheckedChildren="Inactivo"/>
+            </Card>
+        </div>
     )
 }
