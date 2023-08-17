299bbdb5fad690a5a4251601ed12c4165e650e06
Jose Ascurra <joseascurra123@gmail.com>
2023-08-17 14:48:10 -0400

    Cambios en el listado de rifas

--- src/screens/raffles/ListRafflesScreen.tsx
+++ src/screens/raffles/ListRafflesScreen.tsx
@@ -1,5 +1,5 @@
-import { Card, Switch, } from "antd"
-
+import { Card } from "antd";
+import {EditOutlined,DeleteOutlined,ControlOutlined} from '@ant-design/icons';
 export const ListRafflesScreen = () => {
     return (
         <div>
@@ -7,10 +7,15 @@ export const ListRafflesScreen = () => {
                 hoverable
                 style={{ width:240 }}
                 cover={<img alt="imagen de la rifa" src="https://image.slidesharecdn.com/opencontenthistory-1222747121590635-9/95/ten-years-of-open-content-21-728.jpg?cb=1222722154"/>}
+                actions={[
+                    <ControlOutlined key="control"/>,
+                    <EditOutlined key="edit"/>,
+                    <DeleteOutlined key="delete"/>
+                ]}
             >
                 <Card.Meta title="Pollada de Beneficiencia"/>
                 <Card.Meta description="Gs. 20.000" />
-                <Switch checkedChildren="Activo" unCheckedChildren="Inactivo"/>
+                {/* <Switch checkedChildren="Activo" unCheckedChildren="Inactivo"/> */}
             </Card>
         </div>
     )
