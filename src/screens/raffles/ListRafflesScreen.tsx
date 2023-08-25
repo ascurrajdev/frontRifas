3d3156d0eed221148ce3c6c35446828f6eab6d7f
Jose Ascurra <joseascurra123@gmail.com>
2023-08-25 08:46:53 -0400

    Sincronizacion del listado de rifas

--- src/screens/raffles/ListRafflesScreen.tsx
+++ src/screens/raffles/ListRafflesScreen.tsx
@@ -3,31 +3,42 @@ import { Card } from "antd";
 import {EditOutlined,DeleteOutlined,ControlOutlined} from '@ant-design/icons';
 import { useQuery } from "@tanstack/react-query";
 import { listAllRaffles } from "../../services/raffles";
+interface RaffleObject{
+    id: number,
+    description: string,
+    amount: number,
+    created_at: string,
+    updated_at: string,
+    image_url: null | string,
+}
 export const ListRafflesScreen = () => {
     const {data,isLoading} = useQuery({
         queryKey: ['listRaffles'],
         queryFn: listAllRaffles
     })
-    useEffect(() => {
-        console.log(data)
-    },[data])
+    const formatCurrency = (amount: number) => {
+        return `PYG. ${new Intl.NumberFormat('de-DE').format(amount)}`
+    }
     return (
         <div>
             {
-                isLoading ? (
-                    <Card
-                        hoverable
-                        style={{ width:240 }}
-                        cover={<img alt="imagen de la rifa" src="https://image.slidesharecdn.com/opencontenthistory-1222747121590635-9/95/ten-years-of-open-content-21-728.jpg?cb=1222722154"/>}
-                        actions={[
-                            <ControlOutlined key="control"/>,
-                            <EditOutlined key="edit"/>,
-                            <DeleteOutlined key="delete"/>
-                        ]}
-                    >
-                        <Card.Meta title="Pollada de Beneficiencia"/>
-                        <Card.Meta description="Gs. 20.000" />
-                    </Card>
+                !isLoading ? (
+                    data?.data?.map((raffle: RaffleObject) => (
+                        <Card
+                            key={raffle.id}
+                            hoverable
+                            style={{ width:240 }}
+                            cover={<img alt="imagen de la rifa" src={ !!raffle.image_url ? raffle.image_url : "https://image.slidesharecdn.com/opencontenthistory-1222747121590635-9/95/ten-years-of-open-content-21-728.jpg?cb=1222722154"}/>}
+                            actions={[
+                                <ControlOutlined key="control"/>,
+                                <EditOutlined key="edit"/>,
+                                <DeleteOutlined key="delete"/>
+                            ]}
+                        >
+                            <Card.Meta title={raffle.description}/>
+                            <Card.Meta description={formatCurrency(raffle.amount)} />
+                        </Card>
+                    ))
                 ) : (
                     <h1>No hay datos</h1>
                 )
