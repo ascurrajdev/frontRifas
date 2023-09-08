b35af0c07926f9cdb7aa44b6e265b722e616a75d
Jose Ascurra <joseascurra123@gmail.com>
2023-09-08 08:24:00 -0400

    Cambios en la estructura de rutas

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -35,7 +35,7 @@ export function MainRouter(){
                 </Route>
             </Route>
             <Route path="/market/:token" element={<MarketScreen />}/>
-            <Route path='*' element={<NotFound/>}/>
+            <Route path='*' element={<NotFound />}/>
         </Routes>
     )
 }
--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -1,10 +1,15 @@
-import { Result } from "antd";
+import { useNavigate } from "react-router-dom";
+import { Result, Button } from "antd";
 export function NotFound(){
+    const navigate = useNavigate()
     return(
         <Result
             status="404"
             title="404"
             subTitle="Esta pagina no existe o ya expiro."
+            extra={
+                <Button type="primary" onClick={() => navigate(`/`)}>Volver al Inicio</Button>
+            }
         />
     )
 }
